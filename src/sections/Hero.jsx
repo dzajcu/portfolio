5bf5b2514fd45ac04627baff6a4b1e642ceb212a
dzajcu <dawidz1231@gmail.com>
2025-05-13 14:03:13 +0200

    project initialization & first setup

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section id="hero">
+      <div>
+        <h1>Welcome to my portfolio</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
